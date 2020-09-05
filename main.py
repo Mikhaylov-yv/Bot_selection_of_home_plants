0c95bf5fb1c9447a7505caa31d3153fa13650db8
dezmond22 <dezmond22@yandex.ru>
2020-09-06 01:20:22 +0300

    Начал работать

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
-from telegram.ext import Updater, CommandHandler
 import logging
 
+from telegram import (Poll, ParseMode, KeyboardButton, KeyboardButtonPollType,
+                      ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, PollAnswerHandler, PollHandler, MessageHandler,
+                          Filters, ConversationHandler)
 
-token='844125015:AAFnsQim1vBur-OTVphYy-MoQzKNAu2N3eI'
-updater = Updater(token=token,  use_context=True)
-dispatcher = updater.dispatcher
+# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+PODBOR = range(4)
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет парень")
+    """Сообщите пользователю, что умеет этот бот"""
+    update.message.reply_text(
+        'Здоров, я Бот батан.\nМогу распидалить за цвевы для помощи в подборе выбери /podbor')
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-updater.start_polling()
+def podbor(update, context):
+    reply_keyboard = [["Кадый день", "Через день", "Раз в неделю"]]
+    update.message.reply_text(
+        'Здоров, я Бот батан.\nМогу распидалить за цвевы'
+        'Скажи как много времени ты готов уделять зеленым дармоедам?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    print(PODBOR)
+    return PODBOR
 
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("Пользователь %s завершил разговор.", user.first_name)
+    update.message.reply_text('До свидания! Я надеюсь, что когда-нибудь мы сможем снова поговорить.',
+                              reply_markup=ReplyKeyboardRemove())
 
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
+    return ConversationHandler.END
+
+
+def main():
+
+    updater = Updater('844125015:AAFnsQim1vBur-OTVphYy-MoQzKNAu2N3eI', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('podbor', podbor))
+
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
